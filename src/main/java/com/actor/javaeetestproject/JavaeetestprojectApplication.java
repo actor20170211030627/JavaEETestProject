7b1ef8bf0c80dcee0ff1083bfad75648d227405b
actor20170211030627 <1455198886@qq.com>
2020-02-23 13:46:41 +0800

    first commit

---
+++ src/main/java/com/actor/javaeetestproject/JavaeetestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.actor.javaeetestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaeetestprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaeetestprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actor/javaeetestproject/JavaeetestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actor.javaeetestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaeetestprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
